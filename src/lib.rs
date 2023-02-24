96ae7befd6b89b04025057e1d77f7f218b786baf
Daniel Schaefer <dhs@frame.work>
2023-02-24 10:40:34 +0800

    Experiment and modify initialization
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- src/lib.rs
+++ src/lib.rs
@@ -7,24 +7,24 @@ pub mod instruction;
 use crate::instruction::Instruction;
 
 use embedded_hal::blocking::delay::DelayMs;
-use embedded_hal::blocking::spi;
 use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::spi::FullDuplex;
 
 /// ST7735 driver to connect to TFT displays.
 pub struct ST7735<SPI, DC, RST>
 where
-    SPI: spi::Write<u8>,
+    SPI: FullDuplex<u8>,
     DC: OutputPin,
     RST: OutputPin,
 {
     /// SPI
-    spi: SPI,
+    pub spi: SPI,
 
     /// Data/command pin.
-    dc: DC,
+    pub dc: DC,
 
     /// Reset pin.
-    rst: RST,
+    pub rst: RST,
 
     /// Whether the colours are inverted (true) or not (false)
     inverted: bool,
@@ -62,19 +62,12 @@ pub enum Orientation {
 
 impl<SPI, DC, RST> ST7735<SPI, DC, RST>
 where
-    SPI: spi::Write<u8>,
+    SPI: FullDuplex<u8>,
     DC: OutputPin,
     RST: OutputPin,
 {
     /// Creates a new driver instance that uses hardware SPI.
-    pub fn new(
-        spi: SPI,
-        dc: DC,
-        rst: RST,
-        inverted: bool,
-        width: u32,
-        height: u32,
-    ) -> Self {
+    pub fn new(spi: SPI, dc: DC, rst: RST, inverted: bool, width: u32, height: u32) -> Self {
         let display = ST7735 {
             spi,
             dc,
@@ -88,6 +81,57 @@ where
 
         display
     }
+
+    fn clear_zoid(&mut self, color: Rgb565) -> Result<(), ()> {
+        let brightness = ((color.r() as u16) + (color.g() as u16) + (color.b() as u16) / 3) as u8;
+        self.set_pixels_buffered_u8(
+            0,
+            0,
+            self.width as u16 - 1,
+            self.height as u16 - 1,
+            core::iter::repeat(brightness).take((self.width * self.height) as usize),
+        )
+    }
+
+    pub fn fill_contiguous_single_color(
+        &mut self,
+        area: &Rectangle,
+        color: Rgb565,
+    ) -> Result<(), ()> {
+        // Clamp area to drawable part of the display target
+        let drawable_area = area.intersection(&Rectangle::new(Point::zero(), self.size()));
+        let brightness = col_to_bright(color);
+        let colors =
+            core::iter::repeat(brightness).take((area.size.width * area.size.height) as usize);
+        //let colors = area.points()
+        //            .filter(|pos| drawable_area.contains(*pos))
+        //            .map(|_pos| brightness);
+
+        if drawable_area.size != Size::zero() {
+            let ex = (drawable_area.top_left.x + (drawable_area.size.width - 1) as i32) as u16;
+            let ey = (drawable_area.top_left.y + (drawable_area.size.height - 1) as i32) as u16;
+            self.set_pixels_buffered_u8(
+                drawable_area.top_left.x as u16,
+                drawable_area.top_left.y as u16,
+                ex,
+                ey,
+                colors,
+            )?;
+        }
+
+        Ok(())
+    }
+
+    //pub fn read_did<DELAY>(&mut self, delay: &mut DELAY) -> Result<u8, ()>
+    //where
+    //    DELAY: DelayMs<u8>,
+    //{
+    //    self.dc.set_low().map_err(|_| ())?;
+    //    self.spi.send(command as u8).map_err(|_| ())?;
+    //    let res = self.spi.read().map_err(|_| ())?;
+    //    Ok(res)
+    //}
+
     /// Runs commands to initialize the display.
     pub fn init_st7306<DELAY>(&mut self, delay: &mut DELAY) -> Result<(), ()>
     where
@@ -96,33 +140,51 @@ where
         self.hard_reset(delay)?;
         self.write_command(Instruction::SWRESET, &[])?;
         delay.delay_ms(200);
-        self.write_command(Instruction::SLPOUT, &[])?;
-        delay.delay_ms(200);
 
         self.write_command(Instruction::NVMLOADCTRL, &[0x17, 0x02])?;
         self.write_command(Instruction::BSTEN, &[0x01])?;
         self.write_command(Instruction::GCTRL, &[0x0E, 0x0A])?;
         self.write_command(Instruction::VSHPCTRL, &[0x41, 0x41, 0x41, 0x41])?;
-        self.write_command(Instruction::VSLPCTRL, &[0x32, 0x32, 0x32, 0x32,])?;
+        self.write_command(Instruction::VSLPCTRL, &[0x32, 0x32, 0x32, 0x32])?;
         self.write_command(Instruction::VSHNCTRL, &[0x46, 0x46, 0x46, 0x46])?;
         self.write_command(Instruction::VSLNCTRL, &[0x46, 0x46, 0x46, 0x46])?;
         self.write_command(Instruction::FRCTRL, &[0x02])?;
-        self.write_command(Instruction::GTUPEQH, &[0xE5, 0xF6, 0x05, 0x46, 0x77, 0x77, 0x77, 0x77, 0x76, 0x45])?;
-        self.write_command(Instruction::GTUPEQL, &[0x05, 0x46, 0x77, 0x77, 0x77, 0x77, 0x76, 0x45])?;
+        self.write_command(
+            Instruction::GTUPEQH,
+            &[0xE5, 0xF6, 0x05, 0x46, 0x77, 0x77, 0x77, 0x77, 0x76, 0x45],
+        )?;
+        self.write_command(
+            Instruction::GTUPEQL,
+            &[0x05, 0x46, 0x77, 0x77, 0x77, 0x77, 0x76, 0x45],
+        )?;
         self.write_command(Instruction::GTCON, &[0x32, 0x03, 0x1F])?;
         self.write_command(Instruction::SOUEQ, &[0x13])?;
+        // Default: 0x78, manual example: 0x78
         self.write_command(Instruction::GATESET, &[0x78])?;
         self.write_command(Instruction::SLPOUT, &[])?;
         delay.delay_ms(120);
         self.write_command(Instruction::OSCSET, &[0xA6, 0xE9])?;
+        // Default: 0x00, manual example: 0x00
         self.write_command(Instruction::VSHLSEL, &[0x00])?;
-        self.write_command(Instruction::MADCTL, &[0x48])?;
-        self.write_command(Instruction::DTFORM, &[0x10])?;
+        // Default 0x00, manual example: 0x48/0b1001000 (MY, DO)
+        // 0b1001000 =      MY, DO
+        // 0b0001000 =        , DO
+        // 0b1001100 =      MY, DO,    GS (Seems to make it start rom the top)
+        // 0b1011000 =    , MY, DO, MV
+        self.write_command(Instruction::MADCTL, &[0x00])?;
+        // Default: 0x00, manual example: 0x10. bit 0 to enable BPS
+        self.write_command(Instruction::DTFORM, &[0x01])?;
+        // Default: 0x20
         self.write_command(Instruction::GAMAMS, &[0x20])?;
-        self.write_command(Instruction::PNLSET, &[0x00])?;
+        // Default: 0xA0, manual example: 0x00
+        self.write_command(Instruction::PNLSET, &[0xA0])?;
+
+        // Will be overridden by each pixel write
         self.write_command(Instruction::CASET, &[0x00, 0x3B])?;
         self.write_command(Instruction::RASET, &[0x00, 0xEF])?;
+
         //self.write_command(Instruction::TEON, &[0x00])?;
+        // Default: off
         self.write_command(Instruction::AUSOPWRCTRL, &[0xFF])?;
         self.write_command(Instruction::HPM, &[])?;
 
@@ -142,7 +204,8 @@ where
         DELAY: DelayMs<u8>,
     {
         // TODO: Detect if HPM or LPM
-        if true {//mode == HPM {
+        if true {
+            //mode == HPM {
             self.write_command(Instruction::SLPIN, &[])?;
             delay.delay_ms(100);
         } else {
@@ -210,6 +273,14 @@ where
         Ok(())
     }
 
+    pub fn invert_screen(&mut self, inverted: bool) -> Result<(), ()> {
+        if inverted {
+            self.write_command(Instruction::INVON, &[])
+        } else {
+            self.write_command(Instruction::INVOFF, &[])
+        }
+    }
+
     pub fn hard_reset<DELAY>(&mut self, delay: &mut DELAY) -> Result<(), ()>
     where
         DELAY: DelayMs<u8>,
@@ -222,10 +293,10 @@ where
         self.rst.set_high().map_err(|_| ())
     }
 
-    fn write_command(&mut self, command: Instruction, params: &[u8]) -> Result<(), ()> {
+    pub fn write_command(&mut self, command: Instruction, params: &[u8]) -> Result<(), ()> {
         // TODO: Check if same
         self.dc.set_low().map_err(|_| ())?;
-        self.spi.write(&[command as u8]).map_err(|_| ())?;
+        self.spi.send(command as u8).map_err(|_| ())?;
         if !params.is_empty() {
             self.start_data()?;
             self.write_data(params)?;
@@ -233,20 +304,38 @@ where
         Ok(())
     }
 
-    fn start_data(&mut self) -> Result<(), ()> {
+    pub fn start_data(&mut self) -> Result<(), ()> {
+        // TODO: Check if same
         self.dc.set_high().map_err(|_| ())
     }
 
-    fn write_data(&mut self, data: &[u8]) -> Result<(), ()> {
-        self.spi.write(data).map_err(|_| ())
+    pub fn write_data(&mut self, data: &[u8]) -> Result<(), ()> {
+        // TODO: Check if same
+        data.iter().for_each(|d| {
+            self.spi.send(*d);
+        });
+        Ok(())
+    }
+
+    pub fn read_data(&mut self, data: &[u8]) -> Result<u8, ()> {
+        // TODO: Check if same
+        self.spi.read().map_err(|_| ())
     }
 
     /// Writes a data word to the display.
-    fn write_word(&mut self, value: u16) -> Result<(), ()> {
+    pub fn write_word(&mut self, value: u16) -> Result<(), ()> {
         self.write_data(&value.to_be_bytes())
     }
 
-    fn write_words_buffered(&mut self, words: impl IntoIterator<Item = u16>) -> Result<(), ()> {
+    pub fn write_byte(&mut self, value: u8) -> Result<(), ()> {
+        self.write_data(&[value])
+    }
+
+    pub fn write_byte_u16(&mut self, value: u16) -> Result<(), ()> {
+        self.write_data(&[value as u8])
+    }
+
+    pub fn write_words_buffered(&mut self, words: impl IntoIterator<Item = u16>) -> Result<(), ()> {
         let mut buffer = [0; 32];
         let mut index = 0;
         for word in words {
@@ -254,6 +343,23 @@ where
             buffer[index] = as_bytes[0];
             buffer[index + 1] = as_bytes[1];
             index += 2;
+            if index >= buffer.len() {
+                self.write_data(&buffer)?;
+                index = 0;
+            }
+        }
+        self.write_data(&buffer[0..index])
+    }
+
+    pub fn write_words_buffered_u8(
+        &mut self,
+        words: impl IntoIterator<Item = u8>,
+    ) -> Result<(), ()> {
+        let mut buffer = [0; 32];
+        let mut index = 0;
+        for word in words {
+            buffer[index] = word;
+            index += 1;
             if index >= buffer.len() {
                 self.write_data(&buffer)?;
                 index = 0;
@@ -277,14 +383,18 @@ where
     /// Sets the address window for the display.
     pub fn set_address_window(&mut self, sx: u16, sy: u16, ex: u16, ey: u16) -> Result<(), ()> {
         // TODO: Check if same
+        let x_lower = (sx + self.dx); // / 8;
+        let x_upper = (ex + self.dx); // / 8;
+        let y_lower = (sy + self.dy); // / 8;
+        let y_upper = (ey + self.dy); // / 8;
         self.write_command(Instruction::CASET, &[])?;
         self.start_data()?;
-        self.write_word(sx + self.dx)?;
-        self.write_word(ex + self.dx)?;
+        self.write_byte_u16(x_lower)?;
+        self.write_byte_u16(x_upper)?;
         self.write_command(Instruction::RASET, &[])?;
         self.start_data()?;
-        self.write_word(sy + self.dy)?;
-        self.write_word(ey + self.dy)
+        self.write_byte_u16(y_lower)?;
+        self.write_byte_u16(y_upper)
     }
 
     /// Sets a pixel color at the given coords.
@@ -293,8 +403,15 @@ where
         self.set_address_window(x, y, x, y)?;
         self.write_command(Instruction::RAMWR, &[])?;
         self.start_data()?;
-        self.write_word(color)
+        self.write_byte_u16(color)
     }
+    //pub fn set_bw_pixel(&mut self, x: u16, y: u16, color: bool) -> Result<(), ()> {
+    //    // TODO: Check if same
+    //    self.set_address_window(x, y, x, y)?;
+    //    self.write_command(Instruction::RAMWR, &[])?;
+    //    self.start_data()?;
+    //    self.write_byte(color as u8)
+    //}
 
     /// Writes pixel colors sequentially into the current drawing window
     pub fn write_pixels<P: IntoIterator<Item = u16>>(&mut self, colors: P) -> Result<(), ()> {
@@ -302,7 +419,7 @@ where
         self.write_command(Instruction::RAMWR, &[])?;
         self.start_data()?;
         for color in colors {
-            self.write_word(color)?;
+            self.write_byte_u16(color)?;
         }
         Ok(())
     }
@@ -313,6 +430,15 @@ where
         self.write_command(Instruction::RAMWR, &[])?;
         self.start_data()?;
         self.write_words_buffered(colors)
+    }
+
+    pub fn write_pixels_buffered_u8<P: IntoIterator<Item = u8>>(
+        &mut self,
+        colors: P,
+    ) -> Result<(), ()> {
+        self.write_command(Instruction::RAMWR, &[])?;
+        self.start_data()?;
+        self.write_words_buffered_u8(colors)
     }
 
     /// Sets pixel colors at the given drawing window
@@ -339,6 +465,18 @@ where
         self.set_address_window(sx, sy, ex, ey)?;
         self.write_pixels_buffered(colors)
     }
+
+    pub fn set_pixels_buffered_u8<P: IntoIterator<Item = u8>>(
+        &mut self,
+        sx: u16,
+        sy: u16,
+        ex: u16,
+        ey: u16,
+        colors: P,
+    ) -> Result<(), ()> {
+        self.set_address_window(sx, sy, ex, ey)?;
+        self.write_pixels_buffered_u8(colors)
+    }
 }
 
 #[cfg(feature = "graphics")]
@@ -354,11 +492,16 @@ use self::embedded_graphics::{
     primitives::Rectangle,
 };
 
+fn col_to_bright(color: Rgb565) -> u8 {
+    let brightness = ((color.r() as u16) + (color.g() as u16) + (color.b() as u16) / 3) as u8;
+    brightness
+}
+
 #[cfg(feature = "graphics")]
 // TODO: Remove color support from here
 impl<SPI, DC, RST> DrawTarget for ST7735<SPI, DC, RST>
 where
-    SPI: spi::Write<u8>,
+    SPI: FullDuplex<u8>,
     DC: OutputPin,
     RST: OutputPin,
 {
@@ -393,17 +536,26 @@ where
     {
         // Clamp area to drawable part of the display target
         let drawable_area = area.intersection(&Rectangle::new(Point::zero(), self.size()));
+        let colors = area
+            .points()
+            .zip(colors)
+            .filter(|(pos, _color)| drawable_area.contains(*pos))
+            .map(|(_pos, color)| col_to_bright(color));
+        //let colors =
+        //        area.points()
+        //            .zip(colors)
+        //            .filter(|(pos, _color)| drawable_area.contains(*pos))
+        //            .map(|(_pos, color)| RawU16::from(color).into_inner());
 
         if drawable_area.size != Size::zero() {
-            self.set_pixels_buffered(
+            let ex = (drawable_area.top_left.x + (drawable_area.size.width - 1) as i32) as u16;
+            let ey = (drawable_area.top_left.y + (drawable_area.size.height - 1) as i32) as u16;
+            self.set_pixels_buffered_u8(
                 drawable_area.top_left.x as u16,
                 drawable_area.top_left.y as u16,
-                (drawable_area.top_left.x + (drawable_area.size.width - 1) as i32) as u16,
-                (drawable_area.top_left.y + (drawable_area.size.height - 1) as i32) as u16,
-                area.points()
-                    .zip(colors)
-                    .filter(|(pos, _color)| drawable_area.contains(*pos))
-                    .map(|(_pos, color)| RawU16::from(color).into_inner()),
+                ex,
+                ey,
+                colors,
             )?;
         }
 
@@ -411,13 +563,14 @@ where
     }
 
     fn clear(&mut self, color: Self::Color) -> Result<(), Self::Error> {
-        self.set_pixels_buffered(
+        let brightness = ((color.r() as u16) + (color.g() as u16) + (color.b() as u16) / 3) as u8;
+        let _rgb_color = RawU16::from(color).into_inner();
+        self.set_pixels_buffered_u8(
             0,
             0,
             self.width as u16 - 1,
             self.height as u16 - 1,
-            core::iter::repeat(RawU16::from(color).into_inner())
-                .take((self.width * self.height) as usize),
+            core::iter::repeat(brightness).take((self.width * self.height) as usize),
         )
     }
 }
@@ -425,7 +578,7 @@ where
 #[cfg(feature = "graphics")]
 impl<SPI, DC, RST> OriginDimensions for ST7735<SPI, DC, RST>
 where
-    SPI: spi::Write<u8>,
+    SPI: FullDuplex<u8>,
     DC: OutputPin,
     RST: OutputPin,
 {
